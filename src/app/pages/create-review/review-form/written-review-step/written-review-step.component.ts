688a8da6071db0e87178fe4d70fee646fcd0b48a
Justin Scott <Jstman2000@gmail.com>
2024-02-10 13:36:48 -0500

    modified written review step with group and template

--- src/app/pages/create-review/review-form/written-review-step/written-review-step.component.ts
+++ src/app/pages/create-review/review-form/written-review-step/written-review-step.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-written-review-step',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./written-review-step.component.scss']
 })
 export class WrittenReviewStepComponent {
-
+  @Input() writtenReviewGroup!: FormGroup;
+  constructor(){}
 }
