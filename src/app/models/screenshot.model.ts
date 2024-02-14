ee5f3bdf497a2f4a977a35983063762e51669e12
Justin Scott <Jstman2000@gmail.com>
2024-02-13 23:12:21 -0500

    Screenshot model

---
+++ src/app/models/screenshot.model.ts
@@ -0,0 +1,10 @@
+export interface ScreenshotModel {
+    alpha_channel:boolean,
+    aniamted:boolean,
+    checksum:string,
+    game:number,
+    height:number,
+    image_id:number,
+    url:string,
+    width:number,
+}
